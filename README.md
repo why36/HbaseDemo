4776234cd097ab7fea92013e4f716e02a9d0e283
why36 <1075941865@qq.com>
2020-11-27 18:18:46 +0800

    report

--- README.md
+++ README.md
@@ -7,7 +7,9 @@
 我使用了BDkit进行实验，创建容器后，参照HBase官方文档进行了单机模式下的Quick Get-started，比较简单，不再赘述:
 
 ![](figures/1.png)
+
 ![](figures/2.png)
+
 ![](figures/3.png)
 
 然后进行伪分布模式的设置
@@ -27,24 +29,90 @@
 
 ![](figures/7.png)
 
+## 2. 用Java完成
+
+该部分主要是通过调用Hbase的Java API来完成shell操作，基本上是将一个命令封装成一个函数的形式，然后在`main`函数中通过执行函数序列，来达到和执行`shell`命令相同的结果
+
+相关函数的实现如下：
+
+- 创建表
+![](figures/18.png)
+
+- 删除表
+![](figures/19.png)
+
+- 增加数据
+![](figures/20.png)
+
+- 获取数据
+![](figures/21.png)
+
+- 扫描表
+![](figures/22.png)
+
+- 增加列族
+![](figures/23.png)
+
+- “命令”序列
+
+![](figures/24.png)
+
+- 结果
+![](figures/25.png)
+
+![](figures/26.png)
+
+可以看出我们的添加列，列族操作都成功完成
+
+
 
 ## 3. 用shell完成
 
 1. 创建students表
 ![](figures/8.png)
 
-2. 扫描创建后的students表
+1. 扫描创建后的students表
 ![](figures/9.png)
 3. 查询学生来自的省
 ![](figures/10.png)
 4. 增加新的列Courses:English，并添加数据
 ![](figures/11.png)
 
-在这期间出现了HMaster消失的问题，查看日志得知是NameNode的DataNode消失
-![](figures/12.png)
-![](figures/13.png)
+
+
+5. 增加新的列族Contact和新列Contact:Email，并添加数据
 ![](figures/14.png)
 ![](figures/15.png)
+
+6. 删除students表，注意删除前要先将其disable
 ![](figures/16.png)
+
+
+
+## 4. 遇到的问题和解决方案
+
+
+### 4.1 在shell操作时HMaster消失
+
+在shell运行期间，出现了HMaster消失的问题，查看日志得知是NameNode的DataNode消失
+
+![](figures/12.png)
+
+因此我尝试重新启动hdfs，但是遭遇失败...推测是bdkit的问题，故删除容器重新创建，解决
+
+![](figures/13.png)
+
+
+### 4.2 Java程序无法运行
+
+在编写完Java程序后，用`mvn package`将其打包，起初我尝试用`java -jar`命令运行它，但报错，
+
 ![](figures/17.png)
 
+后来发现这个必须要在hadoop下运行，因此改用`hadoop jar`命令，成功运行
+
+
+
+### 5.实验心得
+
+本次实验难度并不高，由于使用了bdkit，也省去了配环境上可能会遇到的问题，遇到的麻烦大多也来自于对Hbase的陌生，在多踩几次坑之后也能应对，总的来说是比较顺利的一次实验。
--- src/main/java/nju/HbaseDemo.java
+++ src/main/java/nju/HbaseDemo.java
@@ -12,11 +12,32 @@ public class HbaseDemo {
     public static Admin admin;
     public static void main(String[] args)throws IOException{
         init();
-        createTable("student",new String[]{"score"});
-        insertData("student","zhangsan","score","English","69");
-        insertData("student","zhangsan","score","Math","86");
-        insertData("student","zhangsan","score","Computer","77");
-        getData("student", "zhangsan", "score","English");
+        createTable("students",new String[]{"ID", "Description", "Courses", "Home"});
+        insertData("students", "001", "Description", "Name", "Li Lei");
+        insertData("students", "001", "Description", "Height", "176");
+        insertData("students", "001", "Courses", "Chinese", "80");
+        insertData("students", "001", "Courses", "Math", "90");
+        insertData("students", "001", "Courses", "Physics", "95");
+        insertData("students", "001", "Home", "Province", "Zhejiang");
+        insertData("students", "002", "Description", "Name", "Han Meimei");
+        insertData("students", "002", "Description", "Height", "183");
+        insertData("students", "002", "Courses", "Chinese", "88");
+        insertData("students", "002", "Courses", "Math", "77");
+        insertData("students", "002", "Courses", "Physics", "66");
+        insertData("students", "002", "Home", "Province", "Beijing");
+        insertData("students", "003", "Description", "Name", "Xiao Ming");
+        insertData("students", "003", "Description", "Height", "162");
+        insertData("students", "003", "Courses", "Chinese", "90");
+        insertData("students", "003", "Courses", "Math", "90");
+        insertData("students", "003", "Courses", "Physics", "90");
+        insertData("students", "003", "Home", "Province", "Shanghai");
+        scanTable("students");
+        getData("students", "001", "Home","Province");
+        insertData("students", "003", "Courses", "English", "100");
+        addNewColumnFamily("students", "Contact");
+        insertData("students", "003", "Contact", "Email", "123@nju.com");
+        scanTable("students");
+        deleteTable("students");
         close();
     }
  
@@ -45,6 +66,7 @@ public class HbaseDemo {
     }
  
     public static void createTable(String myTableName,String[] colFamily) throws IOException {
+        System.out.println("Start creating new Table: " + myTableName);
         TableName tableName = TableName.valueOf(myTableName);
         if(admin.tableExists(tableName)){
             System.out.println("talbe is exists!");
@@ -58,6 +80,17 @@ ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(str)).build();
             admin.createTable(tableDescriptor.build());
         } 
     }
+
+    public static void deleteTable(String myTableName) throws IOException{
+        TableName tableName = TableName.valueOf(myTableName);
+        try {
+            admin.disableTable(tableName);
+            admin.deleteTable(tableName);
+            System.out.println("Delete Table: " + myTableName + "success !");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
  
     public static void insertData(String tableName,String rowKey,String colFamily,String col,String val) throws IOException { 
         Table table = connection.getTable(TableName.valueOf(tableName));
@@ -67,12 +100,51 @@ ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(str)).build();
         table.close(); 
     }
  
-    public static void getData(String tableName,String rowKey,String colFamily, String col)throws  IOException{ 
+    public static void getData(String tableName,String rowKey,String colFamily, String col) throws  IOException{
+        System.out.println("Start getting data... "); 
         Table table = connection.getTable(TableName.valueOf(tableName));
         Get get = new Get(rowKey.getBytes());
         get.addColumn(colFamily.getBytes(),col.getBytes());
         Result result = table.get(get);
-        System.out.println(new String(result.getValue(colFamily.getBytes(),col==null?null:col.getBytes())));
+        System.out.println(rowKey + "'s home province is: " + new String(result.getValue(colFamily.getBytes(),col==null?null:col.getBytes())));
         table.close(); 
+    }
+
+    public static void scanTable(String tableName) throws  IOException{
+        System.out.println("Start scanning Table: " + tableName);
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Scan scan = new Scan();
+        ResultScanner results = table.getScanner(scan);
+        try{
+            for(Result row:results) {
+                for (Cell cell : row.listCells()) {
+                    System.out.println(
+                        "RowKey:"
+                        + Bytes.toString(row.getRow())
+                        + " Family:"
+                        + Bytes.toString(CellUtil.cloneFamily(cell))
+                        + " Qualifier:"
+                        + Bytes.toString(CellUtil.cloneQualifier(cell))
+                        + " Value:"
+                        + Bytes.toString(CellUtil.cloneValue(cell)));
+                }
+            }
+            table.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void addNewColumnFamily(String tableName,String colFamily) throws  IOException{
+        try{
+            System.out.println("Start adding new column family: " + colFamily);
+            TableName table = TableName.valueOf(tableName);
+            admin.disableTable(table);
+            HColumnDescriptor hcd = new HColumnDescriptor(colFamily); //新增列族
+            admin.addColumn(table, hcd);
+            admin.enableTableAsync(table);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
